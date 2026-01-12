25c90c27522cdbcf92984054f13d735beb147325
linearblade <sin_vraal@hotmail.com>
2026-01-12 09:34:01 +0800

    copy clipboard for paths

--- src/detail.js
+++ src/detail.js
@@ -47,7 +47,7 @@ function setDetail(ctx, info) {
 	  canonicalPath !== info.path;
     
     detailEl.innerHTML = `
-    <div style="opacity:0.8;margin-bottom:5px" data-copy-me="info.path" >${escapeHtml(info.path)}</div>
+    <div style="opacity:0.8;margin-bottom:5px"  >${escapeHtml(info.path)} <button data-copy-me>⧉</button> </div>
 
 ${
   showCanonical
